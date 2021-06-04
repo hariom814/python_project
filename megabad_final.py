cb947417e5123b0bc594cc297638c3339fb0a291
hariom814 <49308222+hariom814@users.noreply.github.com>
2021-06-05 02:41:46 +0530

    Add files via upload

--- megabad_final.py
+++ megabad_final.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-from openpyxl import Workbook
 from scrapy import Selector
 import traceback
 import csv
@@ -9,7 +8,7 @@ import requests
 import random
 import re
 from fake_useragent import UserAgent
-ua = UserAgent()
+ua = UserAgent(verify_ssl=False)
 
 headers = {
     'Connection': 'keep-alive',
@@ -24,10 +23,10 @@ headers = {
     'Accept-Language': 'en-US,en;q=0.9',
 }
 
-path = '/home/master/Ibex/megabad'
+path = '/Users/hari/Documents/hari/Crisil/Reuter_31_05/'
 
 
-proxy_list = pd.read_excel(path+"/proxy.xlsx")['proxy'].tolist()
+proxy_list = pd.read_excel(path+"proxy.xlsx")['proxy'].tolist()
 
 def get_proxy():
     proxy = random.choice(proxy_list)
@@ -94,7 +93,7 @@ def get_crawl(response,i):
     except:
         delivery = 'NA'
     print('\n')
-    return ([supp, i, sku, title, price_req, orig_price_req, saving_price_req, m_ean, product_model, mf_name_req, mf_series,delivery])
+    return ([supp, i, sku, title, price_req, orig_price_req, saving_price_req, m_ean, "\""+product_model+"\"", mf_name_req, mf_series,delivery])
 
 def get_response(i):
     retry_var = 0
@@ -103,23 +102,23 @@ def get_response(i):
         try:
             headers['User-Agent'] = ua.random
             # time.sleep(random.randint(0, 2))
-            resp = requests.get(i, verify=False,headers=headers, proxies=get_proxy())
-            # resp = requests.get(i, verify=False, headers=headers)
+            # resp = requests.get(i, verify=False,headers=headers, proxies=get_proxy())
+            resp = requests.get(i, verify=False, headers=headers, proxies=get_proxy())
             print(resp.status_code)
             if resp.status_code == 200:
-                # time.sleep(random.randint(0, 15))
+                time.sleep(random.randint(0, 2))
                 print("using proxy")
                 break
             else:
                 retry_var += 1
-                # time.sleep(random.randint(0, 2))
+                time.sleep(random.randint(0, 2))
                 print("=========>>>In else section Retrying again=======>>>{}".format(retry_var))
-                continue
+                # continue
         except:
             print("Retrying")
             retry_var += 1
             time.sleep(random.randint(0, 1))
-    if retry_var == 5:
+    if retry_var == 11:
         writer.writerow(
             ["missing value", i, resp, "missing value", "missing value", "missing value", "missing value",
              "missing value", "missing value", "missing value", "missing value", "missing value"])
@@ -127,77 +126,30 @@ def get_response(i):
         resp = 0
     return resp
 
+url_list = pd.read_excel(path+"megabad_all_links.xlsx")['Product_url'].tolist()
+url_list = url_list[20000:30000]
 
-
-url_list = pd.read_excel(path+"/megabad_all_links.xlsx")['Product_url'].tolist()
-url_list = url_list[:20000]
-
-# with open(path+'final_mega_product_page_data_1_7k.csv', 'w', newline='', encoding='utf-8') as file:
-#     writer = csv.writer(file)
-#     writer.writerow(["Supplier_ID", "URL","SKU", "Title", "Price", "Original Price", "Savings", "Manufacturer_EAN", "Products_Model", "Manufacturer_name", "Manufacturer_Series", "Shipping_Time"])
-
-file_name = path + "/megabad_1_20K.xlsx"
-book = Workbook()
-sheet = book.active
-
-sheet['A1'] = "Supplier_ID"
-sheet['B1'] = "URL"
-sheet['C1'] = "SKU"
-sheet['D1'] = "Title"
-sheet['E1'] = "Price"
-sheet['F1'] = "Original Price"
-sheet['G1'] = "Savings"
-sheet['H1'] = "Manufacturer_EAN"
-sheet['I1'] = "Products_Model"
-sheet['J1'] = "Manufacturer_name"
-sheet['K1'] = "Manufacturer_Series"
-sheet['L1'] = "Shipping_Time"
-book.save(file_name)
-
-for i in range(0,len(url_list)):
-    print(url_list[i])
-    resp = get_response(url_list[i])
-    if resp == 0:
-        continue
-    response = Selector(text=resp.text)
-    data_main = get_crawl(response,url_list[i])
-    # writer.writerow(data_main)
-    sheet['A{}'.format(i+2)] = data_main[0]
-    sheet['B{}'.format(i+2)] = data_main[1]
-    sheet['C{}'.format(i+2)] = data_main[2]
-    sheet['D{}'.format(i+2)] = data_main[3]
-    sheet['E{}'.format(i+2)] = data_main[4]
-    sheet['F{}'.format(i+2)] = data_main[5]
-    sheet['G{}'.format(i+2)] = data_main[6]
-    sheet['H{}'.format(i+2)] = data_main[7]
-    sheet['I{}'.format(i+2)] = data_main[8]
-    sheet['J{}'.format(i+2)] = data_main[9]
-    sheet['K{}'.format(i+2)] = data_main[10]
-    sheet['L{}'.format(i+2)] = data_main[11]
-    book.save(file_name)
-    try:
-        var_id = list(filter(None, list(set(re.findall('varid=(.*?)\"', resp.text)))))
-    except:
-        var_id = []
-    if var_id:
-        for id in range(0,len(var_id)):
-            link = url_list[i]+"?varid={}".format(var_id[id])
-            resp = get_response(link)
-            if resp == 0:
-                continue
-            response = Selector(text=resp.text)
-            data_main1 = get_crawl(response,link)
-            # writer.writerow(data_main1)
-            sheet['A{}'.format(i + id + 3)] = data_main1[0]
-            sheet['B{}'.format(i + id + 3)] = data_main1[1]
-            sheet['C{}'.format(i + id + 3)] = data_main1[2]
-            sheet['D{}'.format(i + id + 3)] = data_main1[3]
-            sheet['E{}'.format(i + id + 3)] = data_main1[4]
-            sheet['F{}'.format(i + id + 3)] = data_main1[5]
-            sheet['G{}'.format(i + id + 3)] = data_main1[6]
-            sheet['H{}'.format(i + id + 3)] = data_main1[7]
-            sheet['I{}'.format(i + id + 3)] = data_main1[8]
-            sheet['J{}'.format(i + id + 3)] = data_main1[9]
-            sheet['K{}'.format(i + id + 3)] = data_main1[10]
-            sheet['L{}'.format(i + id + 3)] = data_main1[11]
-            book.save(file_name)
+with open(path+'megabad_20k_30K.csv', 'w', newline='', encoding='utf-8-sig') as file:
+    writer = csv.writer(file)
+    writer.writerow(["Supplier_ID", "URL","SKU", "Title", "Price", "Original Price", "Savings", "Manufacturer_EAN", "Products_Model", "Manufacturer_name", "Manufacturer_Series", "Shipping_Time"])
+    for i in url_list:
+        print(i)
+        resp = get_response(i)
+        if resp == 0:
+            continue
+        response = Selector(text=resp.text)
+        data_main = get_crawl(response,i)
+        writer.writerow(data_main)
+        try:
+            var_id = list(filter(None, list(set(re.findall('varid=(.*?)\"', resp.text)))))
+        except:
+            var_id = []
+        if var_id:
+            for id in var_id:
+                link = i+"?varid={}".format(id)
+                resp = get_response(link)
+                if resp == 0:
+                    continue
+                response = Selector(text=resp.text)
+                data_main1 = get_crawl(response,link)
+                writer.writerow(data_main1)
