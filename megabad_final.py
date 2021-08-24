1877c57c730d3280cd30a39e9f3de3c533c9b192
hariom814 <49308222+hariom814@users.noreply.github.com>
2021-08-24 16:26:00 +0530

    Update megabad_final.py

--- megabad_final.py
+++ megabad_final.py
@@ -1,3 +1,27 @@
+# import os
+# import requests
+
+# headers = {
+#     'authority': 'www.argos.co.uk',
+#     'cache-control': 'max-age=0',
+#     'sec-ch-ua': '"Chromium";v="92", " Not A;Brand";v="99", "Google Chrome";v="92"',
+#     'sec-ch-ua-mobile': '?0',
+#     'upgrade-insecure-requests': '1',
+#     'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36',
+#     'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+#     'service-worker-navigation-preload': 'true',
+#     'sec-fetch-site': 'none',
+#     'sec-fetch-mode': 'navigate',
+#     'sec-fetch-user': '?1',
+#     'sec-fetch-dest': 'document',
+#     'accept-language': 'en-US,en;q=0.9,hi;q=0.8',
+
+# }
+
+# response = requests.get('https://www.argos.co.uk/search/smart-tv/smart-tv:yes/opt/sort:customer-rating/',
+#                         headers=headers)
+
+
 # -*- coding: utf-8 -*-
 from scrapy import Selector
 import traceback
